ed46496eb7bba56725c6dd56c98cd7ff68a23b67
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-01-15 23:33:47 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,11 +18,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        A a1 = new A();
+
         B b1 = new B();
         b1.meth();
+        A a1 = new A();
         a1.meth();
-    }
 }
 
   
