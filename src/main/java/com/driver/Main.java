40102d5880e831af02dbea8e82091c4bd82c87f2
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-01-15 23:18:18 +0530

    overrloading solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
-  
+
+   public static class A {
+
+
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    //
+    public static class B extends A {
+        //create object of class B and call method meth of class A with it
+       public String meth() {
+           return "Method is overridden in Extendend class B";
+       }
+    }
+
+    public static void main(String[] args) {
+        A a1 = new A();
+        B b1 = new B();
+        a1.meth();
+        b1.meth();
+    }
 }
+
+  
