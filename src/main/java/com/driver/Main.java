dfcbbd474982afd471ba1d0eed3b48609c27c02e
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-01-16 13:57:42 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
         public String meth() {
-            return "Method is overridden in Extendend class B";
+            return "Invoking method from class A";
         }
     }
 
