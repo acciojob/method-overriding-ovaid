93591e23c173391e374e95022c5593207b493349
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-01-16 12:53:12 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,27 +2,29 @@ package com.driver;
 
 public class Main {
 
-   public static class A {
+    public static class A {
 
 
         public String meth() {
             return "Method is overridden in Extendend class B";
         }
     }
-    //
+
+    //@override
     public static class B extends A {
         //create object of class B and call method meth of class A with it
-       public String meth() {
-           return "Method is overridden in Extendend class B";
-       }
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
     }
 
     public static void main(String[] args) {
 
+        B b = new B();
+        b.meth();
         B b1 = new B();
         b1.meth();
-        A a1 = new A();
-        a1.meth();
+    }
 }
 
   
